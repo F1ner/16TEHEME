ca7272d9d50560bf7d31854b177ce987bd567418
F1ner <123871318+F1ner@users.noreply.github.com>
2024-01-28 11:59:51 +0200

    16.1

---
+++ 16_1ababiy/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _16_1ababiy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //1 ЗАДАНИЕ
+
+            //// Получаем текущую дату и время
+            //DateTime currentDateTime = DateTime.Now;
+
+            //// Выводим информацию в нужных форматах
+            //Console.WriteLine($"Текущая дата:\n{currentDateTime:dd.MM.yyyy}\n{currentDateTime:dd MMM yyyy}\n{currentDateTime:dd MMMM yyyy 'г.'}");
+            //// Форматируем и выводим дату в указанных форматах (по порядку): день.месяц.год, день месяца год, день месяца год с указанием месяца словами
+
+            //Console.WriteLine($"\n{currentDateTime:ddd, dd MMM yyyy}\n{currentDateTime:dddd, dd MMMM yyyy}");
+            //// Форматируем и выводим день недели, день месяца год, полную дату с указанием месяца словами
+
+            //Console.WriteLine("\nТекущее время:\n{0:HH:mm:ss}\n{0:HH:mm}", currentDateTime);
+            //// Форматируем и выводим текущее время в часы:минуты:секунды и часы:минуты
+
+            //Console.WriteLine("\nТекущая дата и время:\n{0:dd.MM.yyyy HH:mm:ss}\n{0:dd MMMM yyyy 'г.' HH:mm}\n{0:dd MMMM yyyy 'г.' HH:mm:ss}", currentDateTime);
+            //// Форматируем и выводим текущую дату и время в указанных форматах: день.месяц.год часы:минуты:секунды, день месяца год часы:минуты, день месяца год часы:минуты:секунды
+
+            //Console.WriteLine("{0:ddd, dd MMM yyyy HH:mm:ss 'GMT'}", currentDateTime);
+            //// Форматируем и выводим день недели, день месяца год часы:минуты:секунды с указанием времени по Гринвичу (GMT)
+
+            //Console.WriteLine("{0:yyyy-MM-ddTHH:mm:ss}", currentDateTime);
+            //// Форматируем и выводим текущую дату и время в формате ISO 8601 (год-месяц-деньTчасы:минуты:секунды)
+
+
+            //2 ЗАДАНИЕ
+
+            //// Получаем текущую дату и время
+            //DateTime today = DateTime.Now;
+
+            //// Выводим приглашение для ввода возраста человека
+            //Console.WriteLine("Введите возраст человека:");
+
+            //// Считываем возраст из ввода пользователя
+            //int age = int.Parse(Console.ReadLine());
+
+            //// Вычисляем год рождения, вычитая возраст из текущего года
+            //int birthYear = today.Year - age;
+
+            //// Выводим год рождения
+            //Console.WriteLine($"Ты родился в {birthYear} году");
+
+
+            //3 ЗАДАНИЕ
+
+            //// Получаем текущую дату и время
+            //DateTime today = DateTime.Now;
+
+            //// Выводим приглашение для ввода года рождения человека
+            //Console.WriteLine("Введите год рождения человека:");
+
+            //// Объявляем переменную для хранения введенной даты
+            //DateTime date;
+
+            //// Считываем введенную дату и записываем ее в переменную date
+            //DateTime.TryParse(Console.ReadLine(), out date);
+
+            //// Вычисляем возраст, вычитая год рождения из текущего года
+            //int age = today.Year - date.Year;
+
+            //// Выводим возраст
+            //Console.WriteLine($"Этому человеку {age} лет");
+
+
+            
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 16_1ababiy/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("16_1ababiy")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("16_1ababiy")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("fe240c1c-e361-40f1-919d-df8f1ec85a5c")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
