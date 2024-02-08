38c8f8dd6d80f81a76b7dd7158d98227a9b9960b
F1ner <123871318+F1ner@users.noreply.github.com>
2024-02-08 20:37:25 +0200

    Add files via upload

---
+++ Ababiy16_4/Employee.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy16_4
+{
+    internal class Employee
+    {
+        // Поля класса
+        public int EmployeeId { get; set; } // табельный номер
+        public string FullName { get; set; } // ФИО сотрудника
+        public DateTime DateOfBirth { get; set; } // дата рождения
+        public string Position { get; set; } // должность
+        public double Salary { get; set; } // оклад
+
+        // Конструктор класса
+        public Employee(int employeeId, string fullName, DateTime dateOfBirth, string position, double salary)
+        {
+            EmployeeId = employeeId;
+            FullName = fullName;
+            DateOfBirth = dateOfBirth;
+            Position = position;
+            Salary = salary;
+        }
+
+        // Метод для вычисления возраста сотрудника
+        public int CalculateAge()
+        {
+            DateTime today = DateTime.Today;
+            int age = today.Year - DateOfBirth.Year;
+            if (DateOfBirth > today.AddYears(-age)) age--;
+            return age;
+        }
+    }
+}
---
+++ Ababiy16_4/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy16_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите количество сотрудников: ");
+            int N = int.Parse(Console.ReadLine()); // Количество сотрудников
+
+            // Создание массива сотрудников
+            Employee[] employees = new Employee[N];
+
+            // Заполнение массива данными с клавиатуры
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine($"\nВведите информацию о {i + 1}-м сотруднике:");
+                Console.Write("Табельный номер: ");
+                int employeeId = int.Parse(Console.ReadLine());
+                Console.Write("ФИО: ");
+                string fullName = Console.ReadLine();
+                Console.Write("Дата рождения (гггг-мм-дд): ");
+                DateTime dateOfBirth = DateTime.Parse(Console.ReadLine());
+                Console.Write("Должность: ");
+                string position = Console.ReadLine();
+                Console.Write("Оклад: ");
+                double salary = double.Parse(Console.ReadLine());
+
+                employees[i] = new Employee(employeeId, fullName, dateOfBirth, position, salary);
+            }
+
+            // Вывод информации о сотрудниках в табличном виде
+            Console.WriteLine("\nИнформация о сотрудниках:");
+            Console.WriteLine("{0,-15} {1,-25} {2,-15} {3,-20} {4,-10}", "Табельный номер", "ФИО", "Дата рождения", "Должность", "Оклад");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("{0,-15} {1,-25} {2,-15:dd.MM.yyyy} {3,-20} {4,-10}", employee.EmployeeId, employee.FullName, employee.DateOfBirth, employee.Position, employee.Salary);
+            }
+
+            // Вывод информации о возрасте каждого сотрудника
+            Console.WriteLine("\nИнформация о возрасте каждого сотрудника:");
+            foreach (var employee in employees)
+            {
+                Console.WriteLine("{0}: {1} лет", employee.FullName, employee.CalculateAge());
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
