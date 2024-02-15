442ff97846c69c0c8b0daf5b61b9b0af8e22ce89
F1ner <123871318+F1ner@users.noreply.github.com>
2024-02-15 19:50:13 +0200

    17_3

---
+++ Ababiy17_3/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ababiy17_3
+{
+    public class Manager : Employee
+    {
+
+        /// <summary>
+        /// поле описывающее колличество продаж в тоннах
+        /// </summary>
+        public double SalesVolume { get; set; }
+
+        /// <summary>
+        /// метод расчета дополнения к зарплате менеджера
+        /// </summary>
+        /// <param name="coefficient"></param>
+        /// <returns></returns>
+        public override double GetIncome(double coefficient)
+        {
+            return base.GetIncome(coefficient) + (SalesVolume > 100 ? MinSalary * 0.01 : 0);
+        }
+    }
+    }
